1bab2fc65e224f00d8356183aa198c39852201f1
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-01 13:15:43 -0300

    docs: 📑  template-string

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "João Paulo";
+const idade = 2022-2002;
+const cidadeDeNascimento = "Brasília";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
