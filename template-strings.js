4e2f78787a51f3fb25e1fad9c7f84e091d263d2c
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-01 13:22:00 -0300

    feat: 🛠  Acrescentando nova funcionalidade

--- template-strings.js
+++ template-strings.js
@@ -1,9 +1,15 @@
-const nome = "João Paulo";
-const idade = 2022-2002;
+const nome = "João";
+const idade = 20;
 const cidadeDeNascimento = "Brasília";
-
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacao)
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
+
+
