33095be76834f9ce929360331d78aceea6be75c9
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-01 12:29:07 -0300

    docs: 📑  console.api e erros

---
+++ console.js
@@ -0,0 +1,14 @@
+// log -> registro
+
+const minhaVar = true;
+
+// console.log(245)
+// console.log("eu sou um texto")
+// console.log(minhaVar)
+
+// tratamento de erro!
+
+console.error('deu erro!')
+console.log(2/0)
+console.log("deu erro");
+console.error(new Error("deu erro"));
